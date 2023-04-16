88c404f4a92225d6553cd74a3f91c273ab76b500
Jason Shaun DMello <jason.dmello21@gmail.com>
2023-04-16 14:01:20 +0530

    Made links clickable to sites for the time being

--- frontend/src/components/Intro_page/Navbar.js
+++ frontend/src/components/Intro_page/Navbar.js
@@ -22,21 +22,21 @@ function Navbar() {
             <input id="menu__toggle" type="checkbox" />
             <label className ="menu__btn" htmlFor="menu__toggle"><span></span></label>
             <span className = "nav-links">
-                <Link to="/" className= "nav-item i0">EDC</Link>
                 <Link to="/" className= "nav-item i1">Home</Link>
                 <div className = "nav-item meet i2">
                   <label htmlFor = "expand-box" className= " meet">Meet</label>
                   <span className = "meet-items">
                     <div className = "meet-item">
-                    Physchiatrist 
+                      <a href='https://www.logintohealth.com/' style={{textDecoration:'none' , color:'black'}}>Physchiatrist </a>
                     </div>
                     <div className = "meet-item">
-                    People 
+                    <a href='https://www.logintohealth.com/' style={{textDecoration:'none' , color:'black'}}>People</a>
                     </div> 
                   </span>
                 </div>
-                <a className= "nav-item i3">Volunteer</a>
-                <a className = "nav-item i4">Games</a>
+                <a className= "nav-item i3" href='https://volunteersindia.org/volunteer-programs-india.htm'>Volunteer</a>
+                <a className = "nav-item i4" href='https://www.mentalup.co/blog/free-brain-games-for-seniors'>Games</a>
+                <a className = "nav-item i4" href = 'https://www.coursera.org/'>Learn</a>
                 <a className="nav-item i5 contact" onClick={logout}>Log Out</a>
             </span>
           </nav>
